090bacbd89c7dcc0243e8642b2a2d5b0c8578f49
Maximilian Mantz <maximilian.mantz@yahoo.de>
2016-05-29 21:04:48 +0200

    deleted unneccessary files

--- src/helpers.js
+++
@@ -1,9 +0,0 @@
-export function createTokenManagerConfig() {
-  return {
-    client_id: '581912277515-2htoi7tfgmcj6o1dbvau9q0tuuaqitot.apps.googleusercontent.com',
-    redirect_uri: 'https://redux-oidc-example.herokuapp.com/callback',
-    scope: 'openid profile',
-    response_type: 'id_token token',
-    authority: 'https://accounts.google.com/o/oauth2/v2/auth'
-  };
-}
