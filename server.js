7c9488fe2d13046376329b98e6c65c9ad99ea4e6
Maximilian Mantz <maximilian.mantz@yahoo.de>
2016-05-29 17:48:39 +0200

    and again...

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(function(request, response, next) {
 app.use(express.static(__dirname + '/dist'))
   .get('/', function (req, res) {
     res.sendFile('index.html', {
-      root: './dist'
+      root: __dirname + '/dist'
     });
 });
 
