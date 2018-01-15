3d4235c386d48647cad09480a7feb2d126ea252b
Maximilian Mantz <maximilian.mantz@yahoo.de>
2018-01-15 21:47:39 +0100

    removed any mentioning of 'sagas' from this file

--- src/storeWithoutSilentRenew.js
+++ src/storeWithoutSilentRenew.js
@@ -2,24 +2,18 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
 import createOidcMiddleware, { createUserManager } from 'redux-oidc';
-import createSagaMiddleware from 'redux-saga'
-import { loadSubscriptionsSaga } from './sagas';
 import reducer from './reducer';
 import userManager from './utils/userManager';
 
 // create the middleware with the userManager
 const oidcMiddleware = createOidcMiddleware(userManager);
 
-const sagaMiddleware = createSagaMiddleware();
-
 const initialState = {};
 
 const createStoreWithMiddleware = compose(
-  applyMiddleware(oidcMiddleware, routerMiddleware(browserHistory), sagaMiddleware)
+  applyMiddleware(oidcMiddleware, routerMiddleware(browserHistory))
 )(createStore);
 
 const store = createStoreWithMiddleware(reducer, initialState);
-
-sagaMiddleware.run(loadSubscriptionsSaga);
 
 export default store;
