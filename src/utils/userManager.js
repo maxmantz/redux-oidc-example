ff3701fcd99c3494f01a81142c43e5268827e58a
Yogesh Surendran <carreraphp@gmail.com>
2023-10-08 02:32:55 +0530

    feat: add instructions to create client id

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -1,6 +1,11 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
+  // Below client was used by maxmantz in his original implementation.
+  // It was not working for me when i tried to run the application
+  // so i had to create a new client id for google auth
+  // below youtube video was helpful in implementing it.
+  // url - https://youtu.be/HtJKUQXmtok?si=n3gPce4vt2rdq7dG
   client_id: '581912277515-8pqeloei552og7pa13iufb57iug8vu9k.apps.googleusercontent.com',
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   response_type: 'token id_token',
