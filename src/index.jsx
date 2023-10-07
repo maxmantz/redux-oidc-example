70e2eeb3ed6e20bbb11556f048ff79c3a9a9cd76
Yogesh Surendran <carreraphp@gmail.com>
2023-10-08 01:44:01 +0530

    build: fix react file extensions to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
-// you'll need this for older browsers
-require("es6-promise").polyfill();
-
 const styles = {
   root: {
     fontFamily: '"Roboto", sans-serif',
